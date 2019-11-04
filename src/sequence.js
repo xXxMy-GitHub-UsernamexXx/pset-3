b81b9e01f408b8f3f6e6fe6fa6bd3ea5804544be
ThemGoodVibes <55405534+xXxMy-GitHub-UsernamexXx@users.noreply.github.com>
2019-11-04 14:35:01 -0500

    Update sequence.js

--- src/sequence.js
+++ src/sequence.js
@@ -11,20 +11,20 @@ const value1 = Number(valueQuestion1.question("\n"));
 const value2 = Number(valueQuestion2.question());
 const value3 = Number(valueQuestion3.question());
 
-if ((value1 < MIN || value1 > MAX) || (value2 < MIN || value2 > MAX) || (value3 < MIN || value3 > MAX)) {
+if (((value1 < MIN || value1 > MAX) || (value2 < MIN || value2 > MAX)) || (value3 < MIN || value3 > MAX)) {
         console.log("\nInvalid.");
 } else if (((Number.isNaN(value1)) || Number.isNaN(value2)) || Number.isNaN(value3)) {
         console.log("\nInvalid.");
-} else if (value1 < value2 && value2 < value3) {
-        console.log("\nStrictly increasing.");
 } else if (value1 == value2 && value2 == value3) {
         console.log("\nEqual.");
+} else if (value1 < value2 && value2 < value3) {
+        console.log("\nStrictly increasing.");
 } else if (value1 > value2 && value2 > value3) {
         console.log("\nStrictly decreasing.");
-} else if () {
-
-} else if (value1 < value2 || value2 < value3) {
+} else if (value1 < value2 && value1 < value3) {
         console.log("\nIncreasing.");
-} else if (value1 > value2 || value2 > value3) {
+} else if (value1 > value2 && value1 > value3) {
         console.log("\nDecreasing.");
+} else {
+        console.log("\nUnordered.");
 };
