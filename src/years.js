136caaae6086ae2bfa56a8b9d6bbd0f619123639
ThemGoodVibes <55405534+xXxMy-GitHub-UsernamexXx@users.noreply.github.com>
2019-11-05 20:44:14 -0500

    Update years.js

--- src/years.js
+++ src/years.js
@@ -7,16 +7,21 @@ let year = Number(readlineSync.question("\nEnter a year: "));
 
 year = Math.round(year);
 let leapStatus = "";
-const yearMessage = `\n${year} is ${leapStatus}a leap year.`;
 
 if (true == Number.isNaN(year)) {
         console.log("\nInvalid.");
 } else if (year <= MIN || year > MAX) {
         console.log("\nInvalid.");
+} else if ((year % 4) > 0) {
+        leapStatus = "not ";
+} else if (((year % 400) > 0) && ((year % 100) == 0)) {
+        leapStatus = "not ";
 } else if ((year % 4) == 0) {
         leapStatus = "";
-        console.log(yearMessage);
-} else if (!((year % 4) == 0)) {
-        leapStatus = "not ";
-        console.log(yearMessage);
 };
+
+const yearMessage = `\n${year} is ${leapStatus}a leap year.`;
+
+if (!((true == Number.isNaN(year)) || (year <= MIN || year > MAX))) {
+        console.log(yearMessage);
+}
